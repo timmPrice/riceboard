dd408c40c7591d67e16652ae923797022c0f6c8d
timmPrice <timmprice6@gmail.com>
2024-06-23 12:09:45 -0400

    reorganizing repository

--- riceboard/keymaps/default/keymap.c
+++ riceboard/keymaps/default/keymap.c
@@ -13,8 +13,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_split_3x5_3( 
         KC_1, KC_2, KC_3, KC_4, KC_5,                     KC_6, KC_7, KC_8, KC_9, KC_0,
-        KC_GRV, KC_MINS, LALT_T(KC_D), LCTL_T(KC_F), KC_LGUI,     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
-        KC_NO, KC_BSLS, KC_LBRC, KC_RBRC, KC_QUOT,            KC_HOME, KC_END, KC_COMM, KC_DOT, KC_SLSH,
+        KC_EQL, KC_MINS, LALT_T(KC_D), LCTL_T(KC_F), KC_LGUI,     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
+        KC_GRV, KC_BSLS, KC_LBRC, KC_RBRC, KC_QUOT,            KC_HOME, KC_END, KC_COMM, KC_DOT, KC_SLSH,
         LSFT_T(KC_DEL), KC_SPC, LT(1, KC_TAB),            LT(0, KC_ESC), LSFT_T(KC_ENT), KC_BSPC
     ),
 };
